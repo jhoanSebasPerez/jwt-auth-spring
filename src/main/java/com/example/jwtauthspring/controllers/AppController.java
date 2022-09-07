1c334c5ae2f05e33aebea80658cdf8fb6b409566
jhoanperez <jhoansebastianperez73@gmail.com>
2022-09-07 09:31:49 -0500

    Add landing controller to say welcome

---
+++ src/main/java/com/example/jwtauthspring/controllers/AppController.java
@@ -0,0 +1,13 @@
+package com.example.jwtauthspring.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+    @GetMapping("/")
+    public String index(){
+        return "Welcome to Spring app";
+    }
+}
